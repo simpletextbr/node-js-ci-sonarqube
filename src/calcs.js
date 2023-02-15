89fbdb7397adde9817751ef90cc2064e0c334918
Wesley Praca <simpletextbr@gmail.com>
2023-02-15 20:40:53 -0300

    initial commit

---
+++ src/calcs.js
@@ -0,0 +1,17 @@
+function sum(a, b){
+  return a + b;
+}
+
+function sub(a, b){
+  return a - b;
+}
+
+function times(a, b){
+  return a * b;
+}
+
+function split(a, b){
+  return a / b;
+}
+
+module.exports = { sum, sub, times, split };
---
+++ src/calcs.test.js
@@ -0,0 +1,18 @@
+const { sum, sub, split, times }  = require('./calcs')
+
+
+test("test function sum to calc a simple sum", () => {
+  expect(sum(21, 2)).toBe(23);
+})
+
+test("test function sub to calc a simple sub", () => {
+  expect(sub(25, 2)).toBe(23);
+})
+
+test("test function times to calc a simple times", () => {
+  expect(times(5, 20)).toBe(100);
+})
+
+test("test function split to calc a simple split", () => {
+  expect(split(100, 10)).toBe(10);
+})
